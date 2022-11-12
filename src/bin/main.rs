532f83c512eef0a870840d259df72e6dc79ed671
Brian Dobbs <bdobbs@sketchdev.io>
2022-11-12 11:15:23 -0600

    Further work on figuring out logic for creating identicon image.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,29 @@
-use identicon_image_service::create_image;
+use identicon_image_service::{create_image, FillCoordinates, CornerCoordinates};
 
 fn main() {
-    let image = create_image();
+
+    let mut pixel_map: Vec<FillCoordinates> = Vec::new();
+    
+    pixel_map.push(FillCoordinates {
+        start_corner: CornerCoordinates { x: 0, y: 0 },
+        stop_corner: CornerCoordinates { x: 10, y: 10 },
+        fill: true
+    });
+
+    pixel_map.push(FillCoordinates {
+        start_corner: CornerCoordinates { x: 10, y: 10 },
+        stop_corner: CornerCoordinates { x: 20, y: 20 },
+        fill: false
+    });
+        
+    pixel_map.push(FillCoordinates {
+        start_corner: CornerCoordinates { x: 20, y: 20 },
+        stop_corner: CornerCoordinates { x: 30, y: 30 },
+        fill: true
+    });
+        
+    
+
+    let image = create_image(pixel_map);
     image.save("test.jpg").expect("Failed to save image.");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,34 @@
 use image::{RgbImage, Rgb};
-use sha2::{Sha256, Sha512, Digest};
+use sha2::{Sha256, Digest};
 
 //struct FillStartStop(u32, u32);
 
-struct FillCoordinates {
-    start_corner: CornerCoordinates,
-    stop_corner: CornerCoordinates
+pub struct FillCoordinates {
+    pub start_corner: CornerCoordinates,
+    pub stop_corner: CornerCoordinates,
+    pub fill: bool
 }
 
-struct CornerCoordinates {
-    x: u32,
-    y: u32
+pub struct CornerCoordinates {
+    pub x: u32,
+    pub y: u32
 }
 
-pub fn create_image() -> RgbImage {
+
+
+pub fn create_image(pixel_map: Vec<FillCoordinates>) -> RgbImage {
     let mut img = RgbImage::new(250, 250);
 
-    let pixel_map: [FillCoordinates; 5] = [
-        FillCoordinates {
-            start_corner: CornerCoordinates { x: 0, y: 0 },
-            stop_corner: CornerCoordinates { x: 10, y: 10 }
-        },
-        FillCoordinates {
-            start_corner: CornerCoordinates { x: 20, y: 20 },
-            stop_corner: CornerCoordinates { x: 30, y: 30 }
-        },
-        FillCoordinates {
-            start_corner: CornerCoordinates { x: 140, y: 140 },
-            stop_corner: CornerCoordinates { x: 150, y: 150 }
-        },
-        FillCoordinates {
-            start_corner: CornerCoordinates { x: 200, y: 200 },
-            stop_corner: CornerCoordinates { x: 210, y: 210 }
-        },
-        FillCoordinates {
-            start_corner: CornerCoordinates { x: 60, y: 60 },
-            stop_corner: CornerCoordinates { x: 70, y: 70 }
-        }
-    ];
     
     for fill in pixel_map.iter() {
-        for x in fill.start_corner.x..fill.stop_corner.x {
-            for y in fill.start_corner.y..fill.stop_corner.y  {
-                img.put_pixel(x, y, Rgb([255, 0, 0]));
+        if fill.fill {
+            for x in fill.start_corner.x..fill.stop_corner.x {
+                for y in fill.start_corner.y..fill.stop_corner.y  {
+                    img.put_pixel(x, y, Rgb([255, 0, 0]));
+                }
             }
         }
+        
     }
 
     
@@ -65,18 +49,7 @@ pub fn hash_input(input: String) -> String {
     return result;
 }
 
-pub fn hash_input_512(input: String) -> String {
-    // create a Sha256 object
-    let mut hasher = Sha512::new();
 
-    // take input test for hashing
-    hasher.update(input);
-
-    // get the hashed string by formatting result of hasher finalize
-    let result: String = format!("{:X}", hasher.finalize());
-
-    return result;
-}
 
 #[cfg(test)]
 mod tests {
@@ -88,9 +61,4 @@ mod tests {
         assert_eq!(result, "7F83B1657FF1FC53B92DC18148A1D65DFC2D4B1FA3D677284ADDD200126D9069");
     }
 
-    #[test]
-    fn test_hash_input_512() {
-        let result: String = hash_input_512("Hello World!".to_string());
-        assert_eq!(result, "861844D6704E8573FEC34D967E20BCFEF3D424CF48BE04E6DC08F2BD58C729743371015EAD891CC3CF1C9D34B49264B510751B1FF9E537937BC46B5D6FF4ECC8");
-    }
 }
